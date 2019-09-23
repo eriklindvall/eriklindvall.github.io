9ae0424e507dda2fa9c58025a321b0af814570a7
eriklindvall <erik.lindvall@gmail.com>
2019-09-23 17:23:10 +0200

    WIP

--- esales_cloud.js
+++ esales_cloud.js
@@ -4,7 +4,7 @@ function fetch()
   var password = $('#password').val();
   $.get({
     url: `https://${clusterId}:${password}@cirrus.esales.apptus.com:35000/accounts/${clusterId}/cluster/https`
-    success: function(data)
+    success: (data) =>
     {
       console.log(data);
     }
