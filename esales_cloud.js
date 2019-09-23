d76da2f8e8f9c26669026b40ba2e51aa119aafaa
eriklindvall <erik.lindvall@gmail.com>
2019-09-23 17:20:25 +0200

    WIP

---
+++ esales_cloud.js
@@ -0,0 +1,20 @@
+function fetch()
+{
+  var clusterId = $('#cluster-id').val();
+  var password = $('#password').val();
+  $.get({
+    url: `https://${clusterId}:${password}@cirrus.esales.apptus.com:35000/accounts/${clusterId}/cluster/https`
+    success: function(data)
+    {
+      console.log(data);
+    }
+  })
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $('#generate-links').click(function() {
+      console.log("Handler for .click() called.");
+      fetch();
+    });
+});
